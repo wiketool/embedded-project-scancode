b75340b2388f93fbd622f191514924580338ac22
wiketool <64351271+wiketool@users.noreply.github.com>
2023-03-21 20:30:37 +0800

    去除条形码的支持

--- app/src/main/java/com/example/qrcode/zxing/decoding/DecodeThread.java
+++ app/src/main/java/com/example/qrcode/zxing/decoding/DecodeThread.java
@@ -52,9 +52,9 @@ final class DecodeThread extends Thread {
 
     if (decodeFormats == null || decodeFormats.isEmpty()) {
     	 decodeFormats = new Vector<BarcodeFormat>();
-    	 decodeFormats.addAll(DecodeFormatManager.ONE_D_FORMATS);
+//    	 decodeFormats.addAll(DecodeFormatManager.ONE_D_FORMATS);
     	 decodeFormats.addAll(DecodeFormatManager.QR_CODE_FORMATS);
-    	 decodeFormats.addAll(DecodeFormatManager.DATA_MATRIX_FORMATS);
+//    	 decodeFormats.addAll(DecodeFormatManager.DATA_MATRIX_FORMATS);
     }
     
     hints.put(DecodeHintType.POSSIBLE_FORMATS, decodeFormats);
